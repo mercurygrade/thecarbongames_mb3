635c5965ffa12b64ffa09c280e98f3fbe10f0169
David Oluyale <oluyaled@gmail.com>
2022-10-12 14:28:47 +0100

    account upgrade rust contract

--- service/src/routes/carbonTokenRoute.ts
+++ service/src/routes/carbonTokenRoute.ts
@@ -1,6 +1,5 @@
 import express from 'express'; 
 import { convertCreditToCarbonToken } from '../controller/carbonTokenController';
-
 const carbonTokenRoute = express()
 carbonTokenRoute.post('/convert-credit',  convertCreditToCarbonToken)
 module.exports = carbonTokenRoute
--- service/src/routes/nftAirdropRoute.ts
+++ service/src/routes/nftAirdropRoute.ts
@@ -1,6 +1,5 @@
 import express from 'express'; 
 import { airdropNFT } from '../controller/nftAirdropController';
-
 const nftAirdropRoute = express()
 nftAirdropRoute.post('/near/nft',  airdropNFT)
 module.exports = nftAirdropRoute
